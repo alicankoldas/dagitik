dd82c1a744a63134a4c96b78e2bef50db645ed05
alicankoldas <koldasali@gmail.com>
2015-11-11 20:27:23 +0200

    odev05 in besinci surumu

--- odev05/message_client.py
+++ odev05/message_client.py
@@ -1,32 +1,101 @@
 __author__ = 'AKOLDAS'
-# basit bir client
-
-__author__ = 'AKOLDAS'
 
 import socket               # Import socket module
 import threading
+from multiprocessing import Queue
+import time
 
-class writeThread_1(threading.Thread):
-    def __init__(self,name):
+
+
+
+
+class ReadThread_Client (threading.Thread):
+    def __init__(self,name,cSocket,threadQueue_1):
         threading.Thread.__init__(self)
-        self.name
+        self.name = name
+        self.cSocket = cSocket
+        self.nickname = ""
+        self.threadQueue_1 = threadQueue_1
 
-    def run (self):
-        string_1 = ''
-        while string_1 != 'bitti':
-            string_1 = raw_input("Enter your input: ")
-            print('input value %s in thread %s' % (string_1, self.name))
-            s.send(string_1)
-            print s.recv(1024)
+    def incoming_parser(self,data):
+        response = ""
+        if data[0:3] == 'HEL':
+            response = data
+        elif data [0:3] == 'REJ' :
+            response = data
+        elif data[0:3] == 'SOK':
+            response = data
+        elif data[0:3] == 'TOC':
+            response = data
+        elif data[0:3] == 'LSA':
+            response == data
+        elif data[0:3] == 'ERR':
+            response = data
+        elif data[0:3] == 'ERL':
+            response = data
+        elif data[0:3] == 'MOK':
+            response = data
+        elif data[0:3] == 'MNO' :
+            response = data
+        elif data[0:3] == 'BYE' :
+            response = data
+        else :
+            #print data
+            data = data.replace("'","").replace("(","").replace(")","").replace(",","")
+            #print data
+            response = data
+        return response
+
+    def run(self) :
+         while True :
+            data = self.cSocket.recv(1024)
+            #print(data)
+            data_1234 = self.incoming_parser(data)
+            #print data_1234
+            self.threadQueue_1.put(data_1234)
+
+class WriteThread_Client (threading.Thread) :
+    def __init__(self,name,csoc,threadQueue_1):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.csoc = csoc
+        self.threadQueue_1 = threadQueue_1
+
+    def run(self):
+            while True :
+
+                if self.threadQueue_1.qsize() == 0 :
+                    string_1 = raw_input("Enter your input: ")
+
+                if self.threadQueue_1.qsize() > 0 :
+                    print "*******"
+                    kuyruk_mesaji = self.threadQueue_1.get()
+                    print kuyruk_mesaji
+                    print "*******"
+
+                self.csoc.send(string_1)
+                    #print('input value %s in thread %s' % (string_1, self.name))
+
+                #if self.threadQueue_1.qsize() > 0:
+
+
+
+
 
 
 
 s = socket.socket()         # Create a socket object
 host = "127.0.0.1" # Get local machine name
-port = 12345               # Reserve a port for your service.
+port = 9996               # Reserve a port for your service.
 s.connect((host, port))
-
-myThreadOb1 = writeThread_1("Thread 1")
+sendQueue = Queue()
+myThreadOb1 = ReadThread_Client("ReadThread",s,sendQueue)
 myThreadOb1.start()
+myThreadOb2 = WriteThread_Client("WriteThread",s,sendQueue)
+myThreadOb2.start()
 myThreadOb1.join()
-s.close
+myThreadOb2.join()
+s.close()
+
+
+
--- odev05/message_server.py
+++ odev05/message_server.py
@@ -5,18 +5,43 @@ import threading
 from multiprocessing import Queue
 import random
 from multiprocessing import Lock
+import time
 
 
 dict = {}
 lock = Lock()
+logFile = "log_file.txt"
+
+class LoggerThread (threading.Thread):
+    def __init__(self,name,logQueue,logFileName):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.logQueue = logQueue
+        self.logFileName = open(logFile,"a")
+
+
+    def log(self,message):
+            t = time.ctime()
+            self.logFileName.write(t + message+"\n")
+            self.logFileName.flush()
+
+    def run (self):
+        self.log("Starting"+" "+self.name)
+        while True :
+            if self.logQueue.qsize() > 0 :
+                message_123 = self.logQueue.get()
+                self.log(message_123)
+        self.log("Exiting"+" "+self.name)
+        self.logFileName.close()
 
 class readThread (threading.Thread):
-    def __init__(self,clientSocket, address,threadQueue,fihrist):
+    def __init__(self,clientSocket, address,threadQueue,fihrist,logQueue):
         threading.Thread.__init__(self)
         self.clientSocket = clientSocket
         self.address = address
         self.threadQueue = threadQueue
         self.fihrist = fihrist
+        self.logQueue = logQueue
 
     def parser(self,data):
             response = ""
@@ -56,6 +81,8 @@ class readThread (threading.Thread):
                 if p ==  0:
                     response = "HEL" +" "+ self.nickname
                     self.fihrist[self.nickname] = self.clientSocket
+                    self.logQueue.put(self.nickname+" "+"has join\n")
+
                 else :
                             response = "REJ"+" "+self.nickname
                 print self.fihrist
@@ -67,6 +94,7 @@ class readThread (threading.Thread):
                 try:
                     del self.fihrist[k]
                     print self.fihrist
+                    self.logQueue.put(self.k+" "+"has left"+"\n")
                 except KeyError:
                     pass
 
@@ -82,15 +110,15 @@ class readThread (threading.Thread):
                 message = data[4:]
                 print message
                 is_there_user = 0
-                response = "SOK" +" "+ message
+                response = "SOK"
                 to_nick = ""
                 for nick , sock in self.fihrist.iteritems() :
                         if sock == self.clientSocket :
                             from_nick = nick
                             is_there_user = 1
-                            #lock.acquire()
+
                             self.threadQueue.put((to_nick,from_nick,message))
-                            #lock.release()
+
 
                 if is_there_user == 0 :
                     response = "ERL"
@@ -110,7 +138,9 @@ class readThread (threading.Thread):
                             from_nick = nick
                             print from_nick
                             is_there_user = 1
+
                             self.threadQueue.put((to_nick,from_nick,message))
+
                 if is_there_user == 1 :
                     response = "MOK"
                 if is_there_user == 0 :
@@ -128,21 +158,27 @@ class readThread (threading.Thread):
        data = ""
        cur_thread = threading.currentThread()
        print "Starting"+ cur_thread.getName()
+       self.logQueue.put("Starting Read"+cur_thread.getName()+"\n")
        while  data != "bitti":
             #print 'Got connection from', self.address,cur_thread.getName()
             data = self.clientSocket.recv(1024)
             print(data)
+
             data_1 = self.parser(data)
+            #lock.acquire()
             self.threadQueue.put(data_1)
+            #lock.release()
+       self.logQueue.put("Exiting Read"+cur_thread.getName()+"\n")
        print "Exiting" + cur_thread.getName()
 
 class writeThread (threading.Thread):
-    def __init__(self,cSocket, address,threadQueue,fihrist):
+    def __init__(self,cSocket, address,threadQueue,fihrist,logQueue):
         threading.Thread.__init__(self)
         self.cSocket = cSocket
         self.address = address
         self.threadQueue = threadQueue
         self.fihrist = fihrist
+        self.logQueue = logQueue
 
     def run(self):
 
@@ -150,12 +186,15 @@ class writeThread (threading.Thread):
        #l.acquire()
        print "Starting"+ cur_thread.getName()
        #l.release()
+       self.logQueue.put("Starting Write"+cur_thread.getName())
        while  True :
 
          if  self.threadQueue.qsize() > 0:
                 sayac = 0
                 tuple_1 = ['LSA','BYE','TIC','HEL','REJ','SOK','MOK','MNO','ERR','ERL']
+                #lock.acquire()
                 queue_message = self.threadQueue.get()
+                #lock.release()
                 print queue_message
 
                 data_1 = queue_message[0:3]
@@ -167,7 +206,8 @@ class writeThread (threading.Thread):
 
                 if sayac == 0 :
                     print "bulundunuz araniyorsunuz"
-                    queue_message_1 = str(queue_message)
+                    queue_message_1 = repr(queue_message)
+                    print "naber"
                     data_2 = queue_message_1.split(',')
                     print data_2[0]
                     if data_2[0] == "(''":
@@ -176,7 +216,11 @@ class writeThread (threading.Thread):
                         for nick, sock in self.fihrist.iteritems() :
                             sock.send(data_2[2])
                     else :
+                        print "neredesin nerde???"
+                        print data_2[0]
+                        data_2[0] = str = data_2[0].replace("'","").replace("(","").replace(")","")
                         for nick, sock in self.fihrist.iteritems() :
+                            print nick
                             if data_2[0] == nick :
                               sock.send(data_2[2])
 
@@ -184,30 +228,50 @@ class writeThread (threading.Thread):
 
 
        print "Exiting" + cur_thread.getName()
+       self.logQueue.put("Exiting Write"+cur_thread.getName())
 
 
 s = socket.socket()         # Create a socket object
 host = "127.0.0.1" # Get local machine name
-port = 9999                # Reserve a port for your service.
+port = 9996                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 threads_1 = []
 threads_2 = []
-threadQ = Queue()
-dict = {}
-s.listen(5)                 # Now wait for client connection.
+threads_3 = []
+logQ = Queue()
+name = "LoggerThread"
+               # Now wait for client connection.
+myThread_3 = LoggerThread(name,logQ,logFile)
+#myThread_3.setDaemon()
+myThread_3.start()
+s.listen(5)
+
+
 while True:
+   logQ.put("Waiting for connection\n")
    c, addr = s.accept()     # Establish connection with client.
-   myThread_1 = readThread(c,addr,threadQ,dict)
-   myThread_2 = writeThread(c,addr,threadQ,dict)
+   logQ.put("Got connection from"+str(c)+" "+str(addr)+"\n")
+   threadQ = Queue()
+
+   myThread_1 = readThread(c,addr,threadQ,dict,logQ)
+   myThread_2 = writeThread(c,addr,threadQ,dict,logQ)
+
    myThread_1.setDaemon(True)
    myThread_1.start()
    myThread_2.setDaemon(True)
    myThread_2.start()
+
    threads_1.append(myThread_1)
    threads_2.append(myThread_2)
+   threads_3.append(myThread_3)
+
+logQ.put("Closing socket "+str(c)+" "+str(addr)+"\n")
 
 for f in threads_1 :
     f.join()
 
 for g in threads_2 :
     g.join()
+
+for h in threads_3 :
+    h.join()
