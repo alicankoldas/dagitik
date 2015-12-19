637a6fde6a46db096777af688ca00b37eab3e09e
alicankoldas <koldasali@gmail.com>
2015-12-19 16:01:27 +0200

    proje ikinci surum, calisan bir peer

--- proje/negotiator.py
+++ proje/negotiator.py
@@ -8,31 +8,68 @@ import time
 import datetime
 import Queue
 
+dict = {}
 
 class Chat_Server(threading.Thread):
-            def __init__(self,name,cSocket, addr,queue):
+            def __init__(self,name,cSocket, addr,queue,fihrist):
                 threading.Thread.__init__(self)
                 self.name = name
                 self.cSocket = cSocket
                 self.addr = addr
                 self.queue = queue
+                self.fihrist = fihrist
                 self.peer_ip = ""
                 self.peer_port = ""
                 self.connect_point = {}
             def run(self):
                 while  True:
                         message = self.cSocket.recv(1024)
+                        #message_1 = str(self.cSocket)
+                        self.fihrist[message] = self.cSocket
+                        print str(self.fihrist)
                         if message:
                             print "Daniel: " + message + ' (' + datetime.datetime.now().strftime('%H:%M:%S') + ')'
                             self.queue.put(message)
 
 
+class Chat_Read_Client(threading.Thread):
+            def __init__(self,name,msg_queue,sockk,fihrist):
+                threading.Thread.__init__(self)
+                self.name = name
+                self.msg_queue = msg_queue
+                self.sockk = sockk
+                self.fihrist = fihrist
+            def run(self):
+                print "deneme_1"
+                while True :
+                        if self.msg_queue.qsize > 0 :
+                                data_1 = self.msg_queue.get()
+                                print data_1
+                                data_2 = data_1.split(" ")
+                                print "deneme_5"
+                                print data_2[1]
+                                for mess, sock in self.fihrist.iteritems() :
+                                    print "deneme_2"
+                                    if data_2[1] == mess :
+                                        print "deneme_van"
+                                        sock.send("OK")
+
+
+                                #self.sockk.send(data_1)
+                                #data_1 = data_1.split(" ")
+                                #print data_1[1]+"merhaba"
+                                #data_1[1].send(data_1[0])
+
+
+
+
 
 class Chat_Client(threading.Thread):
-            def __init__(self,name,queue):
+            def __init__(self,name,queue,msg_queue):
                 threading.Thread.__init__(self)
                 self.name = name
                 self.queue = queue
+                self.msg_queue = msg_queue
                 self.peer_ip = "127.0.0.1"
                 self.peer_port = 12345
             def run(self):
@@ -42,20 +79,26 @@ class Chat_Client(threading.Thread):
                                     data = self.queue.get()
                                     print "Maniel: " + data + ' (' + datetime.datetime.now().strftime('%H:%M:%S') + ')'
                                     try:
-                                        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                                        sock.connect((self.peer_ip,  self.peer_port))
-                                    except socket.error as e :
-                                        print "hata var"
-                                    self.cSocket.send("sunucunun ese cevabi")
-
+                                        client_Socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                        client_Socket.connect((self.peer_ip,  self.peer_port))
+                                    except socket.error as e:
+                                            client_Socket.close()
+                                    mesaj_1 = "cevabi"
+                                    mesaj_2 = data
+                                    mesaj_3 = mesaj_1+" "+mesaj_2
+                                    self.msg_queue.put(mesaj_3)
+                                    client_read = Chat_Read_Client("ClientThread ",msg_queue,client_Socket,dict)
+                                    client_read.start()
 
 
 
 queue = Queue.Queue()
-client_read = Chat_Client("ClientThread ",queue)
-client_read.start()
+msg_queue = Queue.Queue()
+client=Chat_Client("Client",queue,msg_queue)
+client.start()
 #negotiator server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 host = "127.0.0.1"
 port = 12345
 s.bind((host,port))
@@ -65,5 +108,5 @@ while True :
     c,addr = s.accept()
     print "Got a connection from", addr
     threadCounter += 1
-    thread = Chat_Server("ChatServer",c,addr,queue)
+    thread = Chat_Server("ChatServer",c,addr,queue,dict)
     thread.start()
